6595a54a13defae5a5590b38ba6a1d0747daed78
Luis <79276517+Luis-Ipiales@users.noreply.github.com>
2021-02-22 19:34:13 -0500

    TAREA 22/2/21

---
+++ HotelDeber/Caracteristica.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Hotel;
+
+/**
+ *
+ * @author Luis
+ */
+public class Caracteristica {
+    
+    private Integer id;
+    private String nombre;
+    private String descripcion;
+    private Integer cantidad;
+
+    /**
+     * @return the id
+     */
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the descripcion
+     */
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    /**
+     * @param descripcion the descripcion to set
+     */
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    /**
+     * @return the cantidad
+     */
+    public Integer getCantidad() {
+        return cantidad;
+    }
+
+    /**
+     * @param cantidad the cantidad to set
+     */
+    public void setCantidad(Integer cantidad) {
+        this.cantidad = cantidad;
+    }
+}
---
+++ HotelDeber/Persona.java
@@ -0,0 +1,132 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Hotel;
+
+/**
+ *
+ * @author Luis
+ */
+public class Persona {
+    
+    private String nombre;
+    private String apellido;
+    private Integer edad;
+    private String cedulaidentidad;
+    private String genero;
+    private String direccion;
+    private String telefono;
+    
+
+    
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the apellido
+     */
+    public String getApellido() {
+        return apellido;
+    }
+
+    /**
+     * @param apellido the apellido to set
+     */
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    /**
+     * @return the edad
+     */
+    public Integer getEdad() {
+        return edad;
+    }
+
+    /**
+     * @param edad the edad to set
+     */
+    public void setEdad(Integer edad) {
+        this.edad = edad;
+    }
+
+    /**
+  
+
+    /**
+     * @return the cedulaidentidad
+     */
+    public String getCedulaidentidad() {
+        return cedulaidentidad;
+    }
+
+    /**
+     * @param cedulaidentidad the cedulaidentidad to set
+     */
+    public void setCedulaidentidad(String cedulaidentidad) {
+        this.cedulaidentidad = cedulaidentidad;
+    }
+
+    /**
+     * @return the genero
+     */
+    public String getGenero() {
+        return genero;
+    }
+
+    /**
+     * @param genero the genero to set
+     */
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    /**
+     * @return the direccion
+     */
+    public String getDireccion() {
+        return direccion;
+    }
+
+    /**
+     * @param direccion the direccion to set
+     */
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    /**
+     * @return the telefono
+     */
+    public String getTelefono() {
+        return telefono;
+    }
+
+    /**
+     * @param telefono the telefono to set
+     */
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    /**
+     * @return the cliente
+     */
+   
+    }
+
+  
+    
+    
+    
+
